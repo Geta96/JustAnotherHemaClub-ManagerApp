6597191ed829a84c7ffe8633d524cf3132410819
Tamás Gergely <tamas.gergely@bbraun.com>
2026-06-02 14:57:22 +0200

    My Profile bugfix (after cached load)

--- Views/ProfilePage.xaml.cs
+++ Views/ProfilePage.xaml.cs
@@ -5,13 +5,20 @@ namespace JustAnotherHemaClub.Views;
 
 public partial class ProfilePage : ContentPage
 {
+    private readonly ProfileViewModel _vm;
     private readonly IServiceProvider _services;
 
     public ProfilePage(ProfileViewModel vm, IServiceProvider services)
     {
         InitializeComponent();
-        BindingContext = vm;
+        BindingContext = _vm = vm;
         _services = services;
+    }
+
+    protected override async void OnAppearing()
+    {
+        base.OnAppearing();
+        await _vm.LoadAsync();
     }
 
     private async void OnOpenGdprTapped(object? sender, TappedEventArgs e)
